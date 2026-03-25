d77ef2a1bf8178445c4e49a6bc504c893d3f11b6
NetCardVN <170427857+NetCardVN1@users.noreply.github.com>
2026-03-25 18:18:40 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,15 @@
-const CACHE = 'snake-vfinal';
-const ASSETS = ['./', './index.html', './style.css', './game.js', './manifest.json', './192x192.jpeg'];
+const CACHE_NAME = 'snake-v-pwa';
+const assets = ['./', './index.html', './style.css', './game.js', './manifest.json', './192x192.jpeg', './512x512.jpeg'];
 
 self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
 
 self.addEventListener('notificationclick', e => {
     e.notification.close();
-    e.waitUntil(clients.openWindow('/'));
+    e.waitUntil(clients.openWindow('./'));
 });
-
